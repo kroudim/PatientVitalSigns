b6d2b20c56445c12924ce824f9550337a2907d05
kroudim <kroudim@gmail.com>
2025-07-19 11:05:33 +0300

    Add files via upload

---
+++ PatientVitalSign/PatientVitalSigns.Api/Controllers/PatientsController.cs
@@ -0,0 +1,69 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PatientVitalSigns.Application;
+using PatientVitalSigns.Domain;
+using PatientVitalSigns.Infrastucture;
+using System;
+
+namespace PatientVitalSigns.Api;
+
+[ApiController]
+[Route("api/patients")]
+public class PatientsController : ControllerBase
+    {
+    private readonly IMediator _mediator;
+
+    public PatientsController(IMediator mediator)
+        {
+        _mediator = mediator;
+        }
+
+    // GET /patients
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<PatientDto>>> GetPatients()
+        {
+        var result = await _mediator.Send(new GetPatientsQuery());
+        return Ok(result);
+        }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<PatientDto>> GetPatient(int id)
+        {
+        var result = await _mediator.Send(new GetPatientQuery(id));
+        return Ok(result);
+        }
+
+    // GET /patients/{id}/vitals
+    [HttpGet("{id}/vitals")]
+  public async Task<ActionResult<VitalSignsDto>> GetPatientVitals(int id)
+    {
+    var result = await _mediator.Send(new GetPatientVitalsQuery(id));
+    if (result == null)
+      return NotFound();
+    return Ok(result);
+    }
+
+    [HttpGet("GetAllVitalsCsv")]
+    public async Task<ActionResult<byte[]>> GetAllVitalsCsv()
+        {
+        var exportdata = await _mediator.Send(new GetAllPatientVitalsQuery());
+        if (exportdata == null)
+            return NotFound();
+        
+        var file = new CsvExporter().ExportEventsToCsv(exportdata.ToList());
+
+        return Ok(file);
+        }
+
+    // POST /patients/{id}/vitals
+    [HttpPost("{id}/vitals")]
+  public async Task<ActionResult<PostPatientVitalsResultDto>> PostPatientVitals(int id, [FromBody] PostPatientVitalsDto vitals)
+    {
+    var command = new CreatePatientVitalsCommand(id, vitals);
+    var result = await _mediator.Send(command);
+    if (!result.Success)
+      return BadRequest(result);
+    return Ok(result);
+    }
+  }
---
+++ PatientVitalSign/PatientVitalSigns.Api/StartupExtensions.cs
@@ -0,0 +1,74 @@
+﻿using PatientVitalSigns.Application;
+using PatientVitalSigns.Persistence;
+using Microsoft.EntityFrameworkCore;
+using PatientVitalSigns.Infrastucture;
+
+
+namespace PatientVitalSigns.Api
+    {
+    public static class StartupExtensions
+    {
+        public static WebApplication ConfigureServices(
+        this WebApplicationBuilder builder)
+        {
+
+            builder.Services.AddApplicationServices();
+            builder.Services.AddPersistenceServices(builder.Configuration);
+            builder.Services.AddInfrastructureServices(builder.Configuration);
+            
+
+            builder.Services.AddHttpContextAccessor();
+
+            builder.Services.AddControllers();
+
+            return builder.Build();
+
+        }
+
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "GloboTicket Ticket Management API");
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            
+            app.UseAuthentication();
+
+           // app.UseCustomExceptionHandler();
+
+            app.UseCors("Open");
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            return app;
+
+        }
+
+        public static async Task ResetDatabaseAsync(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            try
+            {
+          var db = scope.ServiceProvider.GetRequiredService<PatientVitalSignsContext>();
+          db.Database.EnsureCreated();
+          DbInitializer.Initialize(db);
+            }
+            catch (Exception ex)
+            {
+                var logger = scope.ServiceProvider.GetRequiredService<ILogger>();
+                logger.LogError(ex, "An error occurred while migrating the database.");
+            }
+        }
+    }
+}
